03270c80d04672b84deae37697e5156ae56905f0
Daniel Carbajal <danielcarbajal@Daniels-MacBook-Pro.local>
2024-06-25 00:51:35 -0500

    feature: Base Auth Setup

---
+++ src/infrastructure/auth/jwt_decode.py
@@ -0,0 +1,18 @@
+from jwt.jwk import RSAJWK
+from jwt import JWT
+from httpx import get
+from jwt.exceptions import JWTDecodeError
+from ...domain.exceptions import NonAuthorize
+from ...config import AUTH0_URL
+
+JWKS = get(f"https://{AUTH0_URL}/.well-known/jwks.json").json()
+rsa_jwt = RSAJWK.from_dict(JWKS["keys"][0])
+jwt = JWT()
+
+
+def jwt_decode(token: str):
+    try:
+        payload = jwt.decode(token, rsa_jwt)
+        return payload
+    except JWTDecodeError as e:
+        raise NonAuthorize("Invalid token")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 from .infrastructure import UnitOfWork, get_db
+from .infrastructure.auth import has_access
 
 app = FastAPI()
 
@@ -15,3 +16,8 @@ def healthcheck():
 def get_books(db: Session = Depends(get_db)):
     uow = UnitOfWork(db)
     return uow.credential_source_repository.get_all()
+
+
+@app.get("/who")
+def who(user: Annotated[dict, Depends(has_access)]):
+    return user
